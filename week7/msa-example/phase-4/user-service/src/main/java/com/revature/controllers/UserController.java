bffb8a499df4fc4f29200e1bb3ab9f9fd8133fdc
Kevin Tran Huu <kevin.tranhuu@revature.com>
2022-07-28 11:11:36 -0400

    adding config server and setting up for messaging queues

--- week7/msa-example/phase-4/user-service/src/main/java/com/revature/controllers/UserController.java
+++ week7/msa-example/phase-4/user-service/src/main/java/com/revature/controllers/UserController.java
@@ -8,12 +8,14 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.revature.clients.TaskClient;
 import com.revature.dtos.TaskDTO;
 import com.revature.models.Role;
+import com.revature.models.Task;
 import com.revature.models.User;
 import com.revature.repositories.UserRepository;
 
@@ -29,6 +31,22 @@ public class UserController {
 	public UserController(UserRepository ur, TaskClient tc) {
 		this.ur = ur;
 		this.tc = tc;
+	}
+	
+	// /users/1/tasks - PUT
+	@PutMapping("/{id}/tasks")
+	public ResponseEntity<User> assignTasks(@PathVariable("id")int id, @RequestBody List<Task> tasks){
+		User user = ur.findById(id).orElse(null);
+
+		if (user == null) {
+			return ResponseEntity.notFound().build();
+		}
+
+		user.setTasks(tasks);
+		
+		ur.save(user);
+		
+		return ResponseEntity.ok(user);
 	}
 
 	@GetMapping
